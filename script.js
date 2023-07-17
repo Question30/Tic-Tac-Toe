13ef13bf87244183b9924278192e13ad0a8056fc
Alexander <alexgomezgomez15@gmail.com>
2023-07-17 16:47:08 -0400

    added readme

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Grabbing h2
+let h2 = document.getElementById('prompt');
 
 //Getting the cell
 let cellA1 = document.getElementById("a1");
@@ -14,6 +16,7 @@ let cellC3 = document.getElementById("c3");
 const cells = [cellA1, cellA2,  cellA3,
 cellB1, cellB2, cellB3, 
 cellC1, cellC2, cellC3]
+
 //Getting the Grid
 const grid = document.getElementById("grid");
 
@@ -21,10 +24,18 @@ const grid = document.getElementById("grid");
 class Player {
     constructor (name, computer){
         this.name = name;
-        this.isComputer = computer;
+        this.isComputer = computer; //TODO:: add computer functions play random cell
         this.isWinner = false;
     }
 
+    computerPlay(){
+        if(this.isComputer === true){
+            const tempCells = [];
+            
+        }
+    }
+
+    //Calls checkCell then places x or o in cell
     play(cell){
       if(checkCell(cell) === true){
         cell.textContent = this.name;
@@ -37,17 +48,27 @@ class Player {
 
 
 //Game functions
-    //End the Game
-    function endGame(){
+    //End the Game, deactivates all active cells
+    function endGame(state){
         for (const cell of cells) {
             if(cell.classList.contains('is-active')){
                 cell.classList.remove('is-active');
                 cell.classList.add('is-deactivated');
             }
         }
+        if(state === "player1"){
+            h2.textContent = "Player 1 Wins!!";
+            h2.style.color ="red";
+        }else if(state === "player2"){
+            h2.textContent = "Player 2 Wins!!";
+            h2.style.color ="Green";
+        }else{
+            h2.textContent = "It's a Tie!!";
+            h2.style.color ="blue";
+        }
     }
 
-    //Checks wether the cell is active boolean
+    //Checks wether the cell is active returns a boolean
     function checkCell(cell){
         if(cell.classList.contains('is-active')){
             console.log("cell is available"); 
@@ -63,7 +84,7 @@ class Player {
     //Resets the game board
     function  resetBoard(){
         for (const cell of cells) {
-            //removes all text and sets the cells back to active  
+            //removes all text, sets the cells back to active, resets number of turns, and player 
             cell.textContent = " ";
             cell.classList.remove('is-deactivated');
             cell.classList.add('is-active');
@@ -71,46 +92,47 @@ class Player {
             player2.isWinner = false;
             playerTurn = 0;
             turns = 0;
+            h2.style.color = "black";
         }
         console.log("reseting board");
     }
 
-    //Checks for winner
+    //Checks for every winning condition on both players, and also checks for a tie
     function checkForwinner(){
         if(player1.isWinner === false){
             console.log('we made it');
             if(cellA1.textContent === 'X' && cellA2.textContent === 'X' && cellA3.textContent ==='X'){
                 console.log('Player1 is the winner!');
                 player1.isWinner = true;
-                endGame();
+                endGame("player1");
             }else if(cellB1.textContent == 'X' && cellB2.textContent === 'X' && cellB3.textContent ==='X'){
                 console.log('Player1 is the winner!');
                 player1.isWinner = true;
-                endGame();
+                endGame("player1");
             }else if(cellC1.textContent == 'X' && cellC2.textContent === 'X' && cellC3.textContent ==='X'){
                 console.log('Player1 is the winner!');
                 player1.isWinner = true;
-                endGame();
+                endGame("player1");
             }else if(cellA1.textContent == 'X' && cellB1.textContent === 'X' && cellC1.textContent ==='X'){
                 console.log('Player1 is the winner!');
                 player1.isWinner = true;
-                endGame();
+                endGame("player1");
             }else if(cellA2.textContent == 'X' && cellB2.textContent === 'X' && cellC2.textContent ==='X'){
                 console.log('Player1 is the winner!');
                 player1.isWinner = true;
-                endGame();
+                endGame("player1");
             }else if(cellA3.textContent == 'X' && cellB3.textContent === 'X' && cellC3.textContent==='X'){
                 console.log('Player1 is the winner!');
                 player1.isWinner = true;
-                endGame();
+                endGame("player1");
             }else if(cellA1.textContent == 'X' && cellB2.textContent === 'X' && cellC3.textContent ==='X'){
                 console.log('Player1 is the winner!');
                 player1.isWinner = true;
-                endGame();
+                endGame("player1");
             }else if(cellA3.textContent == 'X' && cellB2.textContent === 'X' && cellC1.textContent ==='X'){
                 console.log('Player1 is the winner!');
                 player1.isWinner = true;
-                endGame();
+                endGame("player1");
             }else{
                 player1.isWinner = false;
             }
@@ -119,27 +141,35 @@ class Player {
             if(cellA1.textContent == 'O' && cellA2.textContent === 'O' && cellA3.textContent ==='O'){
                 cconsole.log('player2 is the winner!');
                 player2.isWinner = true;
+                endGame("player2");
             }else if(cellB1.textContent == 'O' && cellB2.textContent === 'O' && cellB3.textContent ==='O'){
                 console.log('player2 is the winner!');
                 player2.isWinner = true;
+                endGame("player2");
             }else if(cellC1.textContent == 'O' && cellC2.textContent === 'O' && cellC3.textContent ==='O'){
                 console.log('player2 is the winner!');
                 player2.isWinner = true;
+                endGame("player2");
             }else if(cellA1.textContent == 'O' && cellB1.textContent === 'O' && cellC1.textContent ==='O'){
                 console.log('player2 is the winner!');
                 player2.isWinner = true;
+                endGame("player2");
             }else if(cellA2.textContent == 'O' && cellB2.textContent === 'O' && cellC2.textContent ==='O'){
                 console.log('player2 is the winner!');
                 player2.isWinner = true;
+                endGame("player2");
             }else if(cellA3.textContent == 'O' && cellB3.textContent === 'O' && cellC3.textContent ==='O'){
                 console.log('player2 is the winner!');
                 player2.isWinner = true;
+                endGame("player2");
             }else if(cellA1.textContent ==- 'O' && cellB2.textContent === 'O' && cellC3.textContent ==='O'){
                 console.log('player2 is the winner!');
                 player2.isWinner = true;
+                endGame("player2");
             }else if(cellA3.textContent === 'O' && cellB2.textContent === 'O' && cellC1.textContent ==='O'){
                 console.log('player2 is the winner!');
                 player2.isWinner = true;
+                endGame("player2");
             }else{
                 player2.isWinner = false;
             }
@@ -148,12 +178,12 @@ class Player {
         checkForTie();
     }
 
-    //Check for a tie
+    //Check for a tie if exceeding max number of rounds and no winner yet
     function checkForTie(){
         if(turns > 9){
             if(player1.isWinner === false && player2.isWinner === false){
                 console.log('Game is a tie');
-                endGame();
+                endGame("tie");
             }
         }
     }
@@ -163,25 +193,29 @@ const player2 = new Player('O', false);
 
 let playerTurn = 0; // 0 = player 1, 1 = player 2
 let turns = 1;
+h2.textContent = "Player 1(X)'s turn";
 //Game
 grid.addEventListener("click", function(event){
     if(playerTurn === 0){
+        h2.textContent = "Player 2(O)'s turn";
         if(event.target.classList.contains('is-active')){
         console.log("Player 1 turn");
         player1.play(event.target);
         playerTurn = 1;
         turns++;
-        checkForwinner();
+        checkForwinner("player1");
         }else{
             console.log("Try again");
         }
     }else{
+        h2.textContent = "Player 1(X)'s turn"
         if(event.target.classList.contains('is-active')){
         console.log("Player 2 turn");
         player2.play(event.target);
+        event.target.style.color ="green";
        playerTurn = 0;
        turns++;
-       checkForwinner();
+       checkForwinner("player2");
     }else{
         console.log("Try again");
     }
